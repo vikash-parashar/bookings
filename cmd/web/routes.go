c95cf4ed8201e57f215b7bbef57614ab4aaae17b
Vikash Parashar <vikash@nexg.tech>
2023-09-07 23:14:15 +0530

    initial commit

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,7 +19,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
-	mux.Get("/major-suit", handlers.Repo.Majors)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Get("/contact", handlers.Repo.Contact)
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
